483813deddad5d57b1ef7d5329932098acead83e
Alexandre Marcos <alee_ppl@hotmail.com>
2020-09-27 02:38:24 -0300

    jwt as user create

--- src/presentation/routers/sign-up-route.js
+++ src/presentation/routers/sign-up-route.js
@@ -1,12 +1,16 @@
+require('dotenv').config()
 const User = require('../../domain/user')
 const InvalidParamError = require('../../utils/invalid-param-error')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const Validator = require('../../utils/validator')
 const validator = new Validator()
 
 class SignUp {
   async addUser (req, res) {
     let user = null
+    // encrypt
+    req.body.senha = await bcrypt.hash(req.body.senha, 10)
 
     const { nome, email, senha, telefones } = req.body
     // validations
@@ -14,18 +18,17 @@ class SignUp {
     validator.validateEmail(res, email)
     validator.validatePassword(res, senha)
     validator.validatePhone(res, telefones)
-    // encrypt
-    req.body.senha = await bcrypt.hash(req.body.senha, 10)
     // case
     try {
       const validEmail = await User.findOne({ email: email }).exec()
       if (!validEmail) {
+        req.body.token = jwt.sign({ email, senha }, process.env.ACCESS_TOKEN_SECRET)
         user = await User.create(req.body)
       } else {
         return res.status(409).json(new InvalidParamError(email))
       }
 
-      return res.status(201).json(user)
+      return res.status(201).json({ user })
     } catch (err) {
       console.log(err)
     }
